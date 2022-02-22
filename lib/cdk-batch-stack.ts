92f0289fbbfb33ce11a05d7f4ae8df3ca413c86c
Tim Cutts <tim@thecutts.org>
2022-02-22 14:40:19 +0000

    Make the code a bit more sensible and modular

--- lib/cdk-batch-stack.ts
+++ lib/cdk-batch-stack.ts
@@ -7,15 +7,47 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3n from "aws-cdk-lib/aws-s3-notifications";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+
 export class CdkBatchStack extends cdk.Stack {
+  public inputBucket: s3.Bucket;
+  public outputBucket: s3.Bucket;
+  private ecsInstanceRole: iam.Role;
+  private jobDef: batch.JobDefinition;
+  private jobQueue: batch.JobQueue;
+  private bucketArrival: lambda.Function;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // NB Both input and output buckets have their contents deleted when the
-    // stack is destroyed
+    this.createBuckets();
+    this.grantBucketAccessToFargate();
+    this.createBatchEnvironment();
+    this.createTriggerFunction();
 
+    // Output some variables the user will want to know
+    new cdk.CfnOutput(this, "inputBucketName", {
+      value: this.inputBucket.bucketName,
+      description: "Files placed in here will trigger a batch job submission",
+      exportName: "inputBucket",
+    });
+
+    new cdk.CfnOutput(this, "outputBucketName", {
+      value: this.outputBucket.bucketName,
+      description: "Jobs should write output here",
+      exportName: "outputBucket",
+    });
+
+    new cdk.CfnOutput(this, "submissionLogGroup", {
+      value: this.bucketArrival.logGroup.logGroupArn,
+      description:
+        "Logs of batch submission by the lambda function will be here",
+      exportName: "submissionLogGroup",
+    });
+  }
+
+  private createBuckets() {
     // Source of job files
-    const inputBucket = new s3.Bucket(this, "InputBucket", {
+    this.inputBucket = new s3.Bucket(this, "InputBucket", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       versioned: false,
@@ -30,7 +62,7 @@ export class CdkBatchStack extends cdk.Stack {
     });
 
     // Sink for output files
-    const outputBucket = new s3.Bucket(this, "OutputBucket", {
+    this.outputBucket = new s3.Bucket(this, "OutputBucket", {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
       versioned: false,
@@ -50,12 +82,30 @@ export class CdkBatchStack extends cdk.Stack {
         },
       ],
     });
+  }
 
+  private createTaskExecutionRole(): iam.Role {
+    const role = new iam.Role(this, "TaskExecutionRole", {
+      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
+    });
+
+    role.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName(
+        "service-role/AmazonECSTaskExecutionRolePolicy"
+      )
+    );
+
+    return role;
+  }
+
+  private grantBucketAccessToFargate() {
     // Give Fargate ECS instances permission to read and write from the buckets
-    const ecsInstanceRole = this.createTaskExecutionRole();
-    inputBucket.grantRead(ecsInstanceRole);
-    outputBucket.grantWrite(ecsInstanceRole);
+    this.ecsInstanceRole = this.createTaskExecutionRole();
+    this.inputBucket.grantRead(this.ecsInstanceRole);
+    this.outputBucket.grantWrite(this.ecsInstanceRole);
+  }
 
+  private createBatchEnvironment() {
     // VPC for the compute environment
     const vpc = new ec2.Vpc(this, "BatchVPC");
 
@@ -72,7 +122,7 @@ export class CdkBatchStack extends cdk.Stack {
     );
 
     //  A queue to run the jobs
-    const jobQueue = new batch.JobQueue(this, "JobQueue", {
+    this.jobQueue = new batch.JobQueue(this, "JobQueue", {
       computeEnvironments: [
         {
           computeEnvironment: fargateSpotEnvironment,
@@ -82,80 +132,48 @@ export class CdkBatchStack extends cdk.Stack {
     });
 
     // A default job to run
-    const jobDef = new batch.JobDefinition(this, "TestJob", {
+    this.jobDef = new batch.JobDefinition(this, "TestJob", {
       container: {
         image: ecs.ContainerImage.fromAsset("testjob"),
-        executionRole: ecsInstanceRole,
+        executionRole: this.ecsInstanceRole,
       },
       platformCapabilities: [batch.PlatformCapabilities.FARGATE],
     });
+  }
 
+  private createTriggerFunction() {
     // lambda function to respond to events
-    const bucketArrival = new lambda.Function(this, "BucketArrival", {
+    this.bucketArrival = new lambda.Function(this, "BucketArrival", {
       runtime: lambda.Runtime.NODEJS_14_X,
       code: lambda.Code.fromAsset("lambda/bucketarrival"),
       handler: "bucketarrival.handler",
       environment: {
-        JOB_DEFINITION_NAME: jobDef.jobDefinitionName,
-        JOB_QUEUE_NAME: jobQueue.jobQueueName,
-        S3_OUTPUT_BUCKET: outputBucket.bucketName,
+        JOB_DEFINITION_NAME: this.jobDef.jobDefinitionName,
+        JOB_QUEUE_NAME: this.jobQueue.jobQueueName,
+        S3_OUTPUT_BUCKET: this.outputBucket.bucketName,
       },
     });
 
     // the lambda function needs to be able to submit jobs
     const batchSubmitPolicy = new iam.PolicyStatement({
       actions: ["batch:SubmitJob"],
-      resources: [jobDef.jobDefinitionArn, jobQueue.jobQueueArn],
+      resources: [this.jobDef.jobDefinitionArn, this.jobQueue.jobQueueArn],
     });
 
-    bucketArrival.role?.attachInlinePolicy(
+    this.bucketArrival.role?.attachInlinePolicy(
       new iam.Policy(this, "submit-jobs-policy", {
         statements: [batchSubmitPolicy],
       })
     );
 
     // The lambda function can read the bucket (but doesn't actually need to in many cases)
-    inputBucket.grantRead(bucketArrival);
+    this.inputBucket.grantRead(this.bucketArrival);
 
     // Set up the notifications when objects arrive
-    inputBucket.addEventNotification(
+    this.inputBucket.addEventNotification(
       s3.EventType.OBJECT_CREATED,
-      new s3n.LambdaDestination(bucketArrival)
+      new s3n.LambdaDestination(this.bucketArrival)
       // { prefix: "foo", suffix: ".txt" }
     );
-
-    // Output some variables the user will want to know
-    new cdk.CfnOutput(this, "inputBucketName", {
-      value: inputBucket.bucketName,
-      description: "Files placed in here will trigger a batch job submission",
-      exportName: "inputBucket",
-    });
-
-    new cdk.CfnOutput(this, "outputBucketName", {
-      value: outputBucket.bucketName,
-      description: "Jobs should write output here",
-      exportName: "outputBucket",
-    });
-
-    new cdk.CfnOutput(this, "submissionLogGroup", {
-      value: bucketArrival.logGroup.logGroupArn,
-      description:
-        "Logs of batch submission by the lambda function will be here",
-      exportName: "submissionLogGroup",
-    });
-  }
-
-  private createTaskExecutionRole(): iam.Role {
-    const role = new iam.Role(this, "TaskExecutionRole", {
-      assumedBy: new iam.ServicePrincipal("ecs-tasks.amazonaws.com"),
-    });
-
-    role.addManagedPolicy(
-      iam.ManagedPolicy.fromAwsManagedPolicyName(
-        "service-role/AmazonECSTaskExecutionRolePolicy"
-      )
-    );
-
-    return role;
   }
 }
